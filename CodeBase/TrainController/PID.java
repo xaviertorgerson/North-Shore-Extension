3999d099d9b2783c4776a341cbdad11bf083a976
Garrett <gdgrube@gmail.com>
2016-10-10 14:59:42 -0400

    Update PID.java

--- CodeBase/TrainController/PID.java
+++ CodeBase/TrainController/PID.java
@@ -1,30 +1,30 @@
-/*working variables*/
-unsigned long lastTime;
-double Input, Output, Setpoint;
-double errSum, lastErr;
-double kp, ki, kd;
-void Compute()
-{
-   /*How long since we last calculated*/
-   unsigned long now = millis();
-   double timeChange = (double)(now - lastTime);
+import java.util.*;
+
+public class PID{
+
+	/*working variables*/
+	long lastTime;
+	double input, output, setPoint;
+	double errSum, lastErr;
+	double final kp = 1, ki = 1, kd = 1;
+	Date d = new Date();
+
+	public void compute()
+	{
+   		/*How long since we last calculated*/
+   		double now = millis();
+   		double timeChange = (double)(now - lastTime);
+ 	 
+  	 	/*Compute all the working error variables*/
+		double error = setPoint - input;
+   		errSum += (error * timeChange);
+   		double dErr = (error - lastErr) / timeChange;
   
-   /*Compute all the working error variables*/
-   double error = Setpoint - Input;
-   errSum += (error * timeChange);
-   double dErr = (error - lastErr) / timeChange;
+   		/*Compute PID output*/
+   		output = kp * error + ki * errSum + kd * dErr;
   
-   /*Compute PID Output*/
-   Output = kp * error + ki * errSum + kd * dErr;
-  
-   /*Remember some variables for next time*/
-   lastErr = error;
-   lastTime = now;
-}
-  
-void SetTunings(double Kp, double Ki, double Kd)
-{
-   kp = Kp;
-   ki = Ki;
-   kd = Kd;
+   		/*Remember some variables for next time*/
+   		lastErr = error;
+   		lastTime = now;
+	}
 }
