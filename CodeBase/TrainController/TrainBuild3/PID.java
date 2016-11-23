f97a0afdd96639f99b3878b419112d2b389d8a30
Garrett <gdgrube@gmail.com>
2016-11-22 19:30:43 -0500

    change data types to floats

--- CodeBase/TrainController/TrainBuild3/PID.java
+++ CodeBase/TrainController/TrainBuild3/PID.java
@@ -1,18 +1,18 @@
 public class PID{
 
-  final double kp = 500, ki = 0, kd = 0;                        //instance data
-	double errSum = 0, lastErr = 0;
-	double freq;
+  final flaot kp = 500, ki = 0, kd = 0;                        //instance data
+	float errSum = 0, lastErr = 0;
+	float freq;
 
 	public PID(){                                                 //constructor
 	}
 
-	public double update(double curSpd, double setPoint, double timeChange){
-		  double error = setPoint - curSpd;                          //Compute all the working error variables
+	public float update(float curSpd, float setPoint, float timeChange){
+		float error = setPoint - curSpd;                          //Compute all the working error variables
    		errSum += (error * timeChange);
-   		double dErr = (error - lastErr) / timeChange;
+   		float dErr = (error - lastErr) / timeChange;
   
-   		double power = kp * error + ki * errSum + kd * dErr;       //Compute PID output with weight constants
+   		float power = kp * error + ki * errSum + kd * dErr;       //Compute PID output with weight constants
  	 
    		lastErr = error;                                           //Remember some variables for next time
 
