f64312304c9b271b20c7ee9e5363f73b51cd7953
Garrett <gdgrube@gmail.com>
2016-11-30 18:39:27 -0500

    Add files via upload

---
+++ CodeBase/TrainController/TrainBuild6/Train.java
@@ -0,0 +1,52 @@
+public class Train{
+	
+	TrainController tc;
+	
+	public Train(int ID){
+		tc = new TrainController(ID);
+	}
+	
+	//Sent in simulation time in miliseconds
+	public void update(int timePassed){
+		tc.update(timePassed);
+	}
+	
+	public void updateGrade(float newElevation, float newGrade){
+		if(newElevation > tc.tv.tm.elevation)
+			tc.tv.tm.grade = newGrade;
+		else
+			tc.tv.tm.grade = -1 * newGrade;
+		tc.tv.tm.elevation = newElevation;
+	}
+	
+	public void updateRequest(float authority, float speed){
+		tc.tv.tm.authority = authority;
+		tc.tv.tm.setpnt = speed;
+	}
+	
+	public float getDistance(){
+		return tc.tv.tm.currentDistance;
+	}
+	
+	public int curBlock = 0;
+	public int prevBlock = -1;
+	
+	public void setBlock(int block){	
+		if(block != curBlock)
+			prevBlock = curBlock;
+		curBlock = block;
+	}
+	
+	public void setDistance(int dist){
+		tc.tv.tm.currentDistance = dist;
+	}
+	
+	public int psngrCount(){
+		return tc.tv.tm.curPassengers;
+	}
+	
+	public void enterStation(int embarkers, int side){
+		//tc.enterStation(side);
+		tc.tv.enterStation(embarkers);
+	}
+}
