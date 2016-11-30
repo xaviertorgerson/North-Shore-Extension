aa26ea3d7db46c29959ae21b70a6dc76be486df9
jtd45 <jtd45@pitt.edu>
2016-11-29 19:52:14 -0500

    11/29 Newest version of the track_cont_subsys

---
+++ CodeBase/TrackController/trackcont_newest_subsys/BetterList.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author jeff
+ */
+public class BetterList {
+    private class LList{
+        public PLCLogic logic;
+        public int blockNum;
+        public LList next;
+        public boolean found;
+        public LList(LList ll){
+            blockNum=ll.blockNum;
+            next=ll.next;
+            logic=ll.logic;
+            found=false;
+        }
+        public LList(int bn, PLCLogic l){
+            blockNum=bn;
+            logic=l;
+        }
+        public void setFound(boolean f){
+            found=f;
+        }
+    }
+    private LList last;
+    private LList first;
+    public BetterList(int bn,PLCLogic l){
+        first=new LList(bn, l);
+        last=first;
+    }
+    public void add(int bn, PLCLogic l){
+        last.next=new LList(bn, l);
+        last=last.next;
+    }
+    public PLCLogic find(int bn){
+        LList temp=new LList(first);
+        while(temp!=null){
+            if(temp.blockNum==bn && !temp.found){
+                temp.setFound(true);
+                return temp.logic;
+            }
+            temp=temp.next;
+        }
+        return null;
+    }
+    public void resetFind(){
+        LList temp=new LList(first);
+        while(temp!=null){
+            temp.setFound(false);
+            temp=temp.next;
+        }
+    }
+}
