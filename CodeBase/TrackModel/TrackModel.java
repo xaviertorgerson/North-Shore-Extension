b98c2959086556055f5f0238a82e37e83d7134bd
Xavier Torgerson <xaviertorgerson@gmail.com>
2016-12-13 16:11:53 -0500

    Fix go and debug units

--- CodeBase/TrackModel/TrackModel.java
+++ CodeBase/TrackModel/TrackModel.java
@@ -1,6 +1,11 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 /**
  * The TrackModel stores the data representing the physical system
@@ -8,14 +13,54 @@ import java.util.Scanner;
  * @author Xavier Torgerson
  * @since 2016-12-15
  */
-class TrackModel {
-	
+class TrackModel extends JFrame implements ActionListener {
+
+	JPanel panel;
+
+	JComboBox<String> lineCombo;
+	JTextField numberText;
+	JButton viewBlock;
+
+	JLabel description;
+
 	public ArrayList<Line> lineList;
-	
+
 	public TrackModel() {
+
+		super("TrackModel");
+		setSize(1000, 1000);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setLayout( new FlowLayout() );       // set the layout manager
+
+		lineCombo = new JComboBox<>();
+		add(lineCombo);
+
+		numberText = new JTextField(5);
+		add(numberText);
+
+		viewBlock = new JButton("GO");
+		viewBlock.setActionCommand("GO");
+		viewBlock.addActionListener(this);
+		add(viewBlock);
+
+		description = new JLabel("                                                                           ");  // construct a JLabel
+		add(description);                        // add the label to the JFrame
+
 		lineList = new ArrayList<Line>();
 	}
-	
+
+	public void actionPerformed(ActionEvent e) {
+		if("GO".equals(e.getActionCommand())) {
+			String activeLine = lineCombo.getSelectedItem().toString();
+			int activeBlock = Integer.parseInt(numberText.getText());
+			Block testBlock = getBlock(activeLine, activeBlock);
+			description.setText(testBlock.toDisplay());	
+		}
+		else if("LINE".equals(e.getActionCommand())) {
+		}
+	}
+
 	/**
 	 * Update function is called in the main control loop 
 	 * Track Circuit detects trains and Track Signals are passed to trains
@@ -24,41 +69,50 @@ class TrackModel {
 	 * @param dt change in time since last update so trains can determine displacement
 	 */
 	public void update(int dt) {
-	
+
 		//1. Set train speed and authority
 		//2. Set train grade
 		//3. Update trains
 		//4. Update Train Presence
-			//If move blocks
-			//a. Change Previous and Current block for Train
-			//b. Update Displacement
+		//If move blocks
+		//a. Change Previous and Current block for Train
+		//b. Update Displacement
 
 		for(int i = 0; i < lineList.size(); i++) {
 			for(int k = 0; k < lineList.get(i).trainCount(); k++) {
 				Train updateTrain = lineList.get(i).trainList.get(k);
 				Block checkBlock = lineList.get(i).getBlock(updateTrain.curBlock);
-					updateTrain.updateGrade(checkBlock.getCumElevation(), checkBlock.getGrade());
-					if(checkBlock.getGo()) 
-						updateTrain.updateRequest((float)(checkBlock.getAuthority()-(updateTrain.getDistance()*0.000189394)), checkBlock.getSetPointSpeed());
-					updateTrain.update(dt);
-					if(updateTrain.getDistance() > (3.28084*checkBlock.getSize())) {
-						if(checkBlock.getNextBlock().getNumber() != updateTrain.prevBlock) {
-							checkBlock.setTrainPresent(0);
-							checkBlock.getNextBlock().setTrainPresent(updateTrain.getID());
-							updateTrain.setDistance((int)(updateTrain.getDistance()-(3.28084*checkBlock.getSize())));
-							updateTrain.setBlock(checkBlock.getNextBlock().getNumber());
-						}
-						else if(checkBlock.getPreviousBlock().getNumber() != updateTrain.prevBlock) {
-							checkBlock.setTrainPresent(0);
-							checkBlock.getPreviousBlock().setTrainPresent(updateTrain.getID());
-							updateTrain.setDistance((int)(updateTrain.getDistance()-(3.28084*checkBlock.getSize())));
-							updateTrain.setBlock(checkBlock.getPreviousBlock().getNumber());
-						}
+				updateTrain.updateGrade(checkBlock.getCumElevation(), checkBlock.getGrade());
+
+				if(checkBlock.getGo()){
+					float newAuthority = (float)checkBlock.getAuthority()-(updateTrain.getDistance()*0.000189394);
+					System.out.println("CTC says " + checkBlock.getAuthority() + " Train is " + (updateTrain.getDistance*.000189394) + " authority is " + newAuthority);
+					updateTrain.updateRequest(newAuthority, checkBlock.getSetPointSpeed());
+				}
+				else{ 
+					updateTrain.updateRequest(0,0);
+				}
+
+				updateTrain.update(dt);
+
+				if(updateTrain.getDistance() > (3.28084*checkBlock.getSize())) {
+					if(checkBlock.getNextBlock().getNumber() != updateTrain.prevBlock) {
+						checkBlock.setTrainPresent(0);
+						checkBlock.getNextBlock().setTrainPresent(updateTrain.getID());
+						updateTrain.setDistance((int)(updateTrain.getDistance()-(3.28084*checkBlock.getSize())));
+						updateTrain.setBlock(checkBlock.getNextBlock().getNumber());
+					}
+					else if(checkBlock.getPreviousBlock().getNumber() != updateTrain.prevBlock) {
+						checkBlock.setTrainPresent(0);
+						checkBlock.getPreviousBlock().setTrainPresent(updateTrain.getID());
+						updateTrain.setDistance((int)(updateTrain.getDistance()-(3.28084*checkBlock.getSize())));
+						updateTrain.setBlock(checkBlock.getPreviousBlock().getNumber());
 					}
 				}
-			} 			
-		}
-			
+			}
+		} 			
+	}
+
 	/**
 	 * Loads Blocks from a .csv file in the format provided
 	 * Load switches to connect track sections
@@ -67,7 +121,7 @@ class TrackModel {
 	 * @param file Filename of (.csv) file with block data
 	 */
 	public void loadBlocks(String file) {
-	
+		System.out.println("Loading Blocks");	
 		//Read file
 		try (BufferedReader br = new BufferedReader(new FileReader(file))){
 			String fileLine;
@@ -80,13 +134,13 @@ class TrackModel {
 		catch(Exception e) {
 			System.out.println(e.getClass());
 		}
-	
+
 		for(int i = 0; i < lineList.size(); i++) {
 			lineList.get(i).loadDirection(); //Parse arrow data for allowable directions of travel 
 			lineList.get(i).linkBlocks(); //Link blocks from block data
 			lineList.get(i).loadSwitches(); //Load switches from block data
 		}
-		
+
 	}
 
 	/**
@@ -103,7 +157,9 @@ class TrackModel {
 			index++;
 		}
 		if (index == lineList.size()) {
-			lineList.add(new Line(newBlock.getLine()));		
+			lineList.add(new Line(newBlock.getLine()));
+			System.out.println(newBlock.getLine());
+			lineCombo.addItem(new String(newBlock.getLine()));
 		}
 		lineList.get(index).addBlock(newBlock);
 	}
@@ -195,11 +251,11 @@ class TrackModel {
 			System.out.print("What block would you like to inspect? ");
 			int inspectBlock = user_input.nextInt();
 			user_input.nextLine();	
-			
+
 			System.out.print("What line? ");
 			String inspectLine = user_input.nextLine();
 			Block selectedBlock = track.getBlock(inspectLine, inspectBlock);
-			
+
 			if(selectedBlock != null){	
 				selectedBlock.inspect();
 			}
@@ -214,10 +270,11 @@ class TrackModel {
 
 		TrackModel track = new TrackModel();
 		track.loadBlocks("trackData.csv");
-	
+		track.setVisible(true);	
 		//Train newTrain = new Train(10);
-		track.addTrain(6,track.getBlock("Green",152));
-		inspect(track);
+		//track.addTrain(6,track.getBlock("Green",152));
+
+		//inspect(track);
 	}
 
 }
