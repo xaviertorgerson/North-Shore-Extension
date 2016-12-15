7659334fa9f59f04ffd4f605dd1ec925a3bf99fd
jtd45 <jtd45@pitt.edu>
2016-12-15 12:13:27 -0500

    new

--- CodeBase/TrackController/BetterList.java
+++ CodeBase/TrackController/BetterList.java
@@ -7,7 +7,11 @@
  *
  * @author jeff
  */
+//A simple linked list class used to make explicit block plc logic instructions easy to locate
+//essentially ties an instruction to a block and allows the user to quickly find the instruction using the block number
+//Also allows the user to enter an unspecified number of block tied PLC instructions
 public class BetterList {
+    //Simple linked list class, links together nodes that contain a block number and PLC logic for that block number
     private class LList{
         public PLCLogic logic;
         public int blockNum;
@@ -33,10 +37,12 @@ public class BetterList {
         first=new LList(bn, l);
         last=first;
     }
+    //add a new PLC logic that is tied to the given block number
     public void add(int bn, PLCLogic l){
         last.next=new LList(bn, l);
         last=last.next;
     }
+    //find the PLC logic for a given block number
     public PLCLogic find(int bn){
         LList temp=new LList(first);
         while(temp!=null){
--- CodeBase/TrackController/SwitchStateSuggestion.java
+++ CodeBase/TrackController/SwitchStateSuggestion.java
@@ -6,11 +6,15 @@
  *
  * @author jeff
  */
+
+//Class to store the switch state suggestions for 1 given switch given by the CTC office to control the route of a train
 public class SwitchStateSuggestion {
-    int blockNum;
-    boolean[] state;
-    int[] trainNum;
-	int numberofTrains = 1;
+    int blockNum; //block number of the switch
+    //the state and trainNum arrays are linked by index meaning that the train with trainNum (trainID) at index 1 will have
+    //  the state at index 1 (meaning that if the switch detects this train (index 1) it will go to state at index 1)
+    boolean[] state; //state of the switch (F for state0, T for state 1)
+    int[] trainNum; //train numbers
+    int numberofTrains = 1; //current number of trains on the track
     public SwitchStateSuggestion(int bn, boolean[] s, int[] tn){
         blockNum=bn;
         state=s;
